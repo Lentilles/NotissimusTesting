4fada7464e38d40b4f9a7f2408e940a71ef9852c
Alexey <Marveleks@gmail.com>
2023-02-11 14:18:11 +0300

    TryDoGood

---
+++ NotissimusTesting.DAL/Converter/xml2classes.cs
@@ -0,0 +1,43 @@
+﻿using NotissimusTesting.DAL.Models.Offers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace NotissimusTesting.DAL.Converter
+{
+    public class xml2classes
+    {
+        private Dictionary<string, Type> XML2ClassType = new Dictionary<string, Type>()
+        {
+            {"vendor.model", typeof(VendorModel)},
+            {"book", typeof(Book) },
+            {"audiobook", typeof(AudioBook) },
+            {"artist.title", typeof(ArtistTitle) },
+            {"tour", typeof(Tour) },
+            {"event-ticket", typeof(EventTicket) }
+        };
+
+        public List<Offer> ConvertToOffers()
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("../YML.xml");
+
+            foreach(XmlNode node in xDoc.DocumentElement.ChildNodes)
+            {
+                var XMLType = node.Attributes["type"].Value;
+
+                Type type;
+                if(!XML2ClassType.TryGetValue(XMLType, out type))
+                {
+                    throw new Exception($"{nameof(XML2ClassType)} Doesnt contain {XMLType} key");
+                }
+            }
+            // Так а почему опер
+
+            return new List<Offer>();
+        }
+    }
+}
--- NotissimusTesting.DAL/Models/Offers/OfferElement.cs
+++ NotissimusTesting.DAL/Models/Offers/OfferElement.cs
@@ -1,22 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml;
-using System.Xml.Serialization;
 
 namespace NotissimusTesting.DAL.Models.Offers
 {
-    public class Offer
+    public  class OfferElement
     {
         [Key]
         public int Id { get; set; }
-        List<OfferElement> elements = new List<OfferElement>();
-
-
-
+        public int OfferId { get; set; }
+        public string ElementName { get; set; }
+        public string Value { get; set; }
     }
 }
